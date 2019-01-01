34de03b8f8216c47d1ed26cd145081a828897635
riza.satyabudhi <riza.ega.s@mail.ugm.ac.id>
2019-01-01 12:28:27 +0700

    Delete item Mutation

---
+++ components/DeleteItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class DeleteItem extends Component {
+  render() {
+    return (
+      <button>Delete Item</button>
+    )
+  }
+}
+
+
+export default DeleteItem
